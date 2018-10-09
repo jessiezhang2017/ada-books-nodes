a48b056c5566cd4eb249072d7c53b7613e4b7c9d
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-08 20:20:37 -0700

    updated

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,9 +1,12 @@
 require "test_helper"
+require 'date'
 
 describe Book do
-  let(:book) { Book.new }
+  let(:book) {
+    Book.new title: 'War and Peace', author_id: Author.first.id, description: 'blah', date_published: Date.parse('2018-10-09')
+  }
 
   it "must be valid" do
-    value(book).must_be :valid?
+    expect(book).must_be :valid?
   end
 end
